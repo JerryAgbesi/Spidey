19296aefd959b6f2125eab7dccc3c8cc55bfb46c
JerryAgbesi <jerryeagbesi@gmail.com>
2023-11-14 18:46:12 +0000

    initial commit

---
+++ bin/spidey.dart
@@ -0,0 +1,5 @@
+import 'package:spidey/spidey.dart' as spidey;
+
+void main(List<String> arguments) {
+  print('Hello world: ${spidey.calculate()}!');
+}
---
+++ test/spidey_test.dart
@@ -0,0 +1,8 @@
+import 'package:spidey/spidey.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
