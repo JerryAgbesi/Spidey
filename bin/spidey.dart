ee04e0a36ff546754917ced671a179e472ea71b3
JerryAgbesi <jerryeagbesi@gmail.com>
2023-11-14 19:57:26 +0000

    add basic implementation with hardcoded paths

--- bin/spidey.dart
+++ bin/spidey.dart
@@ -1,5 +1,31 @@
-import 'package:spidey/spidey.dart' as spidey;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${spidey.calculate()}!');
+  generateAssetPath();
+}
+
+void generateAssetPath() async {
+  final String assetPath = 'lib/assets.dart';
+  final Directory assetDir = Directory("assets/");
+
+  final File generatedFile = File(assetPath);
+
+generatedFile
+      .writeAsStringSync("//THIS IS A GENERATED FILE - DO NOT EDIT BY HAND\n");
+  generatedFile.writeAsStringSync("",mode: FileMode.append);
+
+  generatedFile.writeAsStringSync("class Asset{ \n",mode: FileMode.append);
+  generatedFile.writeAsStringSync("",mode: FileMode.append);
+
+  final List<FileSystemEntity> dirContent = assetDir.listSync();
+
+  for (var entity in dirContent) {
+    if (entity is File) {
+    final String fileName = entity.path.split("/").last;
+    final String assetName = fileName.split(".").first;
+    generatedFile.writeAsStringSync("static const String $assetName = \"${entity.path}\";\n",mode: FileMode.append);
+      
+    }
+  }
+  generatedFile.writeAsStringSync("}",mode: FileMode.append);
 }
